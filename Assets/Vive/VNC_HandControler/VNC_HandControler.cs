80924eebc2a90a436cb5492070ead9b445ad2d41
Christophe Floutier <cfloutier@gmail.com>
2017-03-02 15:17:34 +0100

    Vive mouse compile but not tested

--- Assets/Vive/VNC_HandControler/VNC_HandControler.cs
+++ Assets/Vive/VNC_HandControler/VNC_HandControler.cs
@@ -10,6 +10,9 @@ using UnityVncSharp.Unity;
 public class VNC_HandControler : MonoBehaviour
 {
     EVRButtonId mainButton = EVRButtonId.k_EButton_SteamVR_Trigger;
+    EVRButtonId rightButton = EVRButtonId.k_EButton_SteamVR_Touchpad;
+    EVRButtonId midButton = EVRButtonId.k_EButton_Grip;
+
 
     // Use this for initialization
     void Start ()
@@ -34,10 +37,10 @@ public class VNC_HandControler : MonoBehaviour
     SteamVR_Controller.Device controller;
     Transform endLine;
     Transform startLine;
-
+    Hand hand;
     IEnumerator StartUp()
     {
-        Hand hand = GetComponentInParent<Hand>();
+        hand = GetComponentInParent<Hand>();
         Debug.AssertFormat(hand != null, "VNCHandControler shoudl be place child of a Hand");
 
         while (hand.controller == null || hand.controller.index < 0)
@@ -65,35 +68,40 @@ public class VNC_HandControler : MonoBehaviour
         if (controller == null)
             return;
 
+        if (hand.currentAttachedObject != null)
+        {
+            line.gameObject.SetActive(false);
+            return;
+        }
+        else
+            line.gameObject.SetActive(true);
+
         Ray ray = new Ray(startLine.position, startLine.forward);
        
         if (Physics.Raycast(ray, out hit, maxDistance))
         {
             endLine.position = hit.point;
 
-
             Collider c = hit.collider;
             if (touchedCollider != c)
             {
-          //      line.color = Color.yellow;
+                line.color = Color.yellow;
 
                 touchedCollider = c;
                 vnc = c.GetComponent<VNCScreen>();
             }
             else
             {
-            //    line.color = Color.cyan;
+                line.color = Color.cyan;
             }
         }
         else
         {
             touchedCollider = null;
             vnc = null;
-      //      line.color = Color.cyan;
+            line.color = Color.cyan;
             endLine.position = startLine.position + startLine.forward * maxDistance;
         }
-
-        //    controller.GetPressDown()
 
         if (down)
         {
@@ -119,6 +127,18 @@ public class VNC_HandControler : MonoBehaviour
             Vector2 axis = controller.GetAxis(EVRButtonId.k_EButton_SteamVR_Trigger);
             line.sizeDot = Mathf.Lerp(minMaxSizeDot.x, minMaxSizeDot.y, 1 - axis.x);
         }
+
+        if (vnc != null)
+        {
+            Vector3 hit_pos = hit.point;
+
+            transform.position = hit_pos;
+            Vector2 uvPos = hit.textureCoord2;
+
+            vnc.UpdateMouse(uvPos, down, controller.GetPress(rightButton), controller.GetPress(midButton));
+            
+        }
+        
 
     }
 }
